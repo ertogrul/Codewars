2bdc0127ad90ce37addaad972cd47d7e10cda83b
Michal Kordyzon <michalkordyzon@gmail.com>
2017-12-17 23:55:55 +0100

    123

--- 3_jaden.py
+++ 3_jaden.py
@@ -2,6 +2,6 @@ def toJadenCase(string):
     for i in string:
         print (i)
 
-quote = "cat at house"
+quote = "cat at house" #
 quote1 = "How can mirrors be real if our eyes aren't real"
 toJadenCase(quote1)
