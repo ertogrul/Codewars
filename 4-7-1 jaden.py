d1401c0d3294ca30773599a3aea1ff930b1ba727
Michal Kordyzon <michalkordyzon@gmail.com>
2017-12-23 00:32:03 +0100

    jaden

---
+++ 4-7-1 jaden.py
@@ -0,0 +1,7 @@
+def toJadenCase(string):
+    string = string.title()
+    return string
+
+
+quote = "How can mirrors be real if our eyes aren't real"
+print (toJadenCase(quote)) # "How Can Mirrors Be Real If Our Eyes Aren't Real")
