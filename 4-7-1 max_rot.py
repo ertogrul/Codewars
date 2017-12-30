bccaa9624fe011898d566316f92fb1f81d1e5c8c
Michal Kordyzon <michalkordyzon@gmail.com>
2017-12-31 00:09:15 +0100

    maxrot done. upgraded to 6kyu :)

--- 4-7-1 max_rot.py
+++ 4-7-1 max_rot.py
@@ -1,46 +1,63 @@
 def max_rot(n):
+    global greatest
+    greatest = 0
     num = (list(str(n)))
     initial_num_length = len(num)
-    print ("iiii", initial_num_length)
+    print ("initial num length ", initial_num_length)
     first_digits = []
-    print (num)
-
-    while (len(first_digits) <= initial_num_length - 2):
+    check_greatest(first_digits, num)
+    while (len(first_digits) <= initial_num_length - 1):
         rotate(num)
-
         first_digits.append(num[0])
         del num[0]
-        print ("first digits: ", first_digits)
-        print (num)
         check_greatest(first_digits, num)
-
-
+    return greatest
 
 def rotate(num):
     temp = num[0]
     del num[0]
     num.append(temp)
-    print (num)
     return num
 
 def check_greatest(f, n):
+    global greatest
     f = f + n
     a = map(str, f)
-    print ("fff", a)
     a = int(''.join(f))
-    print ("fff", a)
-    """
-    b = map(str, n)
-    print ("nnnn", b)
-    b = int(''.join(n))
-    print ("nnnn", b)
-    """
     if (a > greatest):
-        print ("new greatest: ", a)
+        greatest = a
 
-
-greatest = 0
-print (max_rot(38458215))  # 85821534
+# print (max_rot(38458215))  # 85821534
 # print (max_rot(195881031))  # 988103115
 # print (max_rot(896219342))  # 962193428
 # print (max_rot(69418307))  # 94183076
+print (max_rot(507992495))  # 507992495 a wychodzi - 99249557
+
+
+
+"""
+84582153
+85821534
+85215348
+85253481
+85254813
+85254138
+85254183
+
+85821534
+
+
+
+######
+507992495
+79924955
+99249557
+92495579
+92955794
+92957945
+92959457
+92959574
+92959547
+92959547
+"""
+507992495
